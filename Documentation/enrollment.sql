63b4faa8abd7a9663c6c3df5521bafca56b77a96
kirbygeagonia-create <kirbygeagonia@gmail.com>
2026-08-02 08:38:02 -1100

    Regenerate SQL dump with remember_token column for Stage 2 completion

--- Documentation/enrollment.sql
+++ Documentation/enrollment.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3e4fb2728d982cf32b2cf03f5c524ec86c29c991cbb8b224bba57e20f5fa3d3
-size 91418375
+oid sha256:e7aad09f1c90bbb22e9dac9414208248a80fac7fe61f4d4fc22bb41724e800b1
+size 123072
